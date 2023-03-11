ef3786fb0ff653b6ac1b0c5f28f86800cada5339
AlexanderMar21 <54232340+AlexanderMar21@users.noreply.github.com>
2023-03-11 11:37:25 +0100

    add: packages

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col gap-8 w-400px mx-auto justify-center py-10">
+    <h1>Date</h1>
+    <input
+      type="tel"
+      v-model="inputRef"
+      class="border-2 outline-0"
+      :class="{ error: !isValid }"
+      placeholder="dd.mm.yyyy"
+    />
+    <p>
+      {{ value }}
+    </p>
+    <input type="date" v-model="value" />
+    <br />
+    <br />
+
+    <h1>Month</h1>
+    <input
+      type="tel"
+      v-model="monthInput"
+      class="border-2 outline-0"
+      :class="{ error: !isValidMonth }"
+      placeholder="mm.yyyy"
+    />
+    <p>
+      {{ monthValue }}
+    </p>
+    <input type="month" v-model="monthValue" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { useDate } from './composables/datify';
+import { ref } from 'vue';
+
+const value = ref('1999-12-19');
+const monthValue = ref('1999-12');
+const { isValid, inputRef } = useDate(value, 'dd.mm.yyyy');
+const { isValid: isValidMonth, inputRef: monthInput } = useDate(
+  monthValue,
+  'mm.yyyy',
+  'YYYY-MM'
+);
+</script>
---
+++ src/composables/datify.ts
@@ -0,0 +1,42 @@
+import { Ref, watch, ref } from 'vue';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+import { Mask } from 'maska';
+
+export const useDate = (
+  remoteValue: Ref<string>,
+  format: string,
+  parseFormat = 'YYYY-MM-DD'
+) => {
+  const maskPattern = format.replace(/[a-zA-Z]/gi, '#');
+  format = format.toUpperCase();
+  const mask = new Mask({ mask: maskPattern });
+  const isValid = ref(false);
+  const inputRef = ref('');
+  if (remoteValue.value.length === format.length) {
+    const parsed = dayjs(remoteValue.value, parseFormat, true);
+    inputRef.value = parsed.format(format.toUpperCase());
+    isValid.value = parsed.isValid();
+  }
+  watch(remoteValue, (val) => {
+    console.log(val);
+    if (val) {
+      const parsed = dayjs(val, parseFormat, true);
+      inputRef.value = parsed.format(format.toUpperCase());
+    }
+  });
+  watch(inputRef, (val) => {
+    const parsed = dayjs(val, format.toUpperCase(), true);
+    inputRef.value = mask.masked(inputRef.value);
+    isValid.value = parsed.isValid();
+    if (val.length === format.length) {
+      if (!isValid.value) {
+        return;
+      }
+      remoteValue.value = parsed.format(parseFormat.toUpperCase());
+    }
+  });
+
+  return { isValid, inputRef };
+};
